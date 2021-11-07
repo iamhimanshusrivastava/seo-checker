cd092a990f5d2ecd4025ccf80b62a45776e000ab
Himanshu Srivastava <iamhimanshusrivastava@gmail.com>
2021-11-07 22:55:31 +0530

    Project files

---
+++ components/Input.js
@@ -0,0 +1,49 @@
+import { useRef } from "react";
+import router, { useRouter } from "next/router";
+
+import { XIcon } from "@heroicons/react/solid";
+import Modal from "../components/modal";
+
+function Input() {
+  const router = useRouter();
+
+  const domainInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const domain = domainInputRef.current.value;
+
+    router.push(`/?audit=${domain}`);
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="flex flex-row w-full p-4 justify-center mt-10 drawer-end">
+        <div className="card w-[1200px] text-center shadow-2xl lg:card-side bg-[#2aa79b] text-accent-content">
+          <div className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="text-bold">Enter a webpage url:</span>
+              </label>
+              <input
+                type="text"
+                placeholder="domain.com"
+                className="input input-bordered text-black"
+                ref={domainInputRef}
+              />
+              {/* <XIcon
+                className="h-7 sm:mr-3 text-gray-500 cursor-pointer transition duration-100 transform hover:scale-125"
+                onClick={() => (domainInputRef.current.value = "")}
+              /> */}
+            </div>
+            <div className="justify-center card-actions">
+              <button className="btn btn-accent">Audit</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </form>
+  );
+}
+
+export default Input;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      xxxs: "280px",
+      xxs: "320px",
+      xs: "360px",
+      ...defaultTheme.screens,
+    },
     extend: {},
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
 };
