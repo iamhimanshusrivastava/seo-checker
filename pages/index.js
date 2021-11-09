d11eea28889e3220f740b32ddba25e7e567d3629
Himanshu Srivastava <iamhimanshusrivastava@gmail.com>
2021-11-09 10:48:12 +0530

    Fix: Audit Table

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import useSWRImmutable from "swr/immutable";
 
 import Input from "../components/Input";
 import PassedAudit from "../components/PassedAudit";
-import FailedAudit from "../components/OpportunityAudit";
+import OpportunityAudit from "../components/OpportunityAudit";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
@@ -285,7 +285,7 @@ function Home() {
             data.lighthouseResult &&
             data.lighthouseResult.audits["uses-passive-event-listeners"]
               .score !== 1 && (
-              <FailedAudit
+              <OpportunityAudit
                 title="Uses passive listeners to improve scrolling performance"
                 description="Consider marking your touch and wheel event listeners as `passive` to improve your page's scroll performance."
                 link="https://web.dev/uses-passive-event-listeners/"
@@ -294,7 +294,7 @@ function Home() {
           {data &&
             data.lighthouseResult &&
             data.lighthouseResult.audits["legacy-javascript"].score !== 1 && (
-              <FailedAudit
+              <OpportunityAudit
                 title="Avoid serving legacy JavaScript to modern browsers"
                 description="Polyfills and transforms enable legacy browsers to use new JavaScript features. However, many aren't necessary for modern browsers. For your bundled JavaScript, adopt a modern script deployment strategy using module/nomodule feature detection to reduce the amount of code shipped to modern browsers, while retaining support for legacy browsers."
                 link="https://philipwalton.com/articles/deploying-es2015-code-in-production-today/"
@@ -303,7 +303,7 @@ function Home() {
           {data &&
             data.lighthouseResult &&
             data.lighthouseResult.audits["uses-long-cache-ttl"].score !== 1 && (
-              <FailedAudit
+              <OpportunityAudit
                 title="Uses efficient cache policy on static assets"
                 description="A long cache lifetime can speed up repeat visits to your page."
                 link="https://web.dev/uses-long-cache-ttl/"
@@ -312,7 +312,7 @@ function Home() {
           {data &&
             data.lighthouseResult &&
             data.lighthouseResult.audits["unminified-css"].score !== 1 && (
-              <FailedAudit
+              <OpportunityAudit
                 title="Minify CSS"
                 description="Minifying CSS files can reduce network payload sizes."
                 link="https://web.dev/unminified-css/"
@@ -321,7 +321,7 @@ function Home() {
           {data &&
             data.lighthouseResult &&
             data.lighthouseResult.audits["dom-size"].score !== 1 && (
-              <FailedAudit
+              <OpportunityAudit
                 title="Avoids an excessive DOM size"
                 description="A large DOM will increase memory usage, cause longer style calculations, and produce costly layout reflows"
                 link="https://web.dev/dom-size/"
@@ -331,7 +331,7 @@ function Home() {
             data.lighthouseResult &&
             data.lighthouseResult.audits["uses-responsive-images"].score !==
               1 && (
-              <FailedAudit
+              <OpportunityAudit
                 title="Properly size images"
                 description="Serve images that are appropriately-sized to save cellular data and improve load time."
                 link="https://web.dev/uses-responsive-images/"
@@ -341,7 +341,7 @@ function Home() {
             data.lighthouseResult &&
             data.lighthouseResult.audits["efficient-animated-content"].score !==
               1 && (
-              <FailedAudit
+              <OpportunityAudit
                 title="Use video formats for animated content"
                 description="Large GIFs are inefficient for delivering animated content. Consider using MPEG4/WebM videos for animations and PNG/WebP for static images instead of GIF to save network bytes."
                 link="https://web.dev/efficient-animated-content/"
@@ -350,7 +350,7 @@ function Home() {
           {data &&
             data.lighthouseResult &&
             data.lighthouseResult.audits["redirects"].score !== 1 && (
-              <FailedAudit
+              <OpportunityAudit
                 title="Avoid multiple page redirects"
                 description="Redirects introduce additional delays before the page can be loaded."
                 link="https://web.dev/redirects/"
@@ -360,7 +360,7 @@ function Home() {
             data.lighthouseResult &&
             data.lighthouseResult.audits["unminified-javascript"].score !==
               1 && (
-              <FailedAudit
+              <OpportunityAudit
                 title="Minify JavaScript"
                 description="Minifying JavaScript files can reduce payload sizes and script parse time."
                 link="https://web.dev/unminified-javascript/"
@@ -369,7 +369,7 @@ function Home() {
           {data &&
             data.lighthouseResult &&
             data.lighthouseResult.audits["uses-rel-preconnect"].score !== 1 && (
-              <FailedAudit
+              <OpportunityAudit
                 title="Preconnect to required origins"
                 description="Consider adding `preconnect` or `dns-prefetch` resource hints to establish early connections to important third-party origins."
                 link="https://web.dev/uses-rel-preconnect/"
@@ -378,7 +378,7 @@ function Home() {
           {data &&
             data.lighthouseResult &&
             data.lighthouseResult.audits["unused-css-rules"].score !== 1 && (
-              <FailedAudit
+              <OpportunityAudit
                 title="Reduce unused CSS"
                 description="Reduce unused rules from stylesheets and defer CSS not used for above-the-fold content to decrease bytes consumed by network activity."
                 link="https://web.dev/unused-css-rules/"
@@ -387,7 +387,7 @@ function Home() {
           {data &&
             data.lighthouseResult &&
             data.lighthouseResult.audits["unused-javascript"].score !== 1 && (
-              <FailedAudit
+              <OpportunityAudit
                 title="Reduce unused JavaScript"
                 description="Reduce unused JavaScript and defer loading scripts until they are required to decrease bytes consumed by network activity."
                 link="https://web.dev/unused-javascript/"
@@ -397,7 +397,7 @@ function Home() {
             data.lighthouseResult &&
             data.lighthouseResult.audits["server-response-time"].score !==
               1 && (
-              <FailedAudit
+              <OpportunityAudit
                 title="Initial server response time was long"
                 description="Keep the server response time for the main document short because all other requests depend on it."
                 link="https://web.dev/time-to-first-byte/"
@@ -406,7 +406,7 @@ function Home() {
           {data &&
             data.lighthouseResult &&
             data.lighthouseResult.audits["unsized-images"].score !== 1 && (
-              <FailedAudit
+              <OpportunityAudit
                 title="Image elements have explicit `width` and `height`"
                 description="Set an explicit width and height on image elements to reduce layout shifts and improve CLS."
                 link="https://web.dev/optimize-cls/#images-without-dimensions"
@@ -416,7 +416,7 @@ function Home() {
             data.lighthouseResult &&
             data.lighthouseResult.audits["modern-image-formats"].score !==
               1 && (
-              <FailedAudit
+              <OpportunityAudit
                 title="Serve images in next-gen formats"
                 description="Image formats like WebP and AVIF often provide better compression than PNG or JPEG, which means faster downloads and less data consumption."
                 link="https://web.dev/uses-webp-images/"
@@ -426,7 +426,7 @@ function Home() {
             data.lighthouseResult &&
             data.lighthouseResult.audits["uses-optimized-images"].score !==
               1 && (
-              <FailedAudit
+              <OpportunityAudit
                 title="Efficiently encode images"
                 description="Optimized images load faster and consume less cellular data."
                 link="https://web.dev/uses-optimized-images/"
@@ -435,7 +435,7 @@ function Home() {
           {data &&
             data.lighthouseResult &&
             data.lighthouseResult.audits["offscreen-images"].score !== 1 && (
-              <FailedAudit
+              <OpportunityAudit
                 title="Defer offscreen images"
                 description="Consider lazy-loading offscreen and hidden images after all critical resources have finished loading to lower time to interactive."
                 link="https://web.dev/offscreen-images/"
@@ -445,7 +445,7 @@ function Home() {
             data.lighthouseResult &&
             data.lighthouseResult.audits["render-blocking-resources"].score !==
               1 && (
-              <FailedAudit
+              <OpportunityAudit
                 title="Eliminate render-blocking resources"
                 description="Resources are blocking the first paint of your page. Consider delivering critical JS/CSS inline and deferring all non-critical JS/styles."
                 link="https://web.dev/render-blocking-resources/"
@@ -454,7 +454,7 @@ function Home() {
           {data &&
             data.lighthouseResult &&
             data.lighthouseResult.audits["lcp-lazy-loaded"].score !== 1 && (
-              <FailedAudit
+              <OpportunityAudit
                 title="Largest Contentful Paint image was not lazily loaded"
                 description="Above-the-fold images that are lazily loaded render later in the page lifecycle, which can delay the largest contentful paint."
                 link="https://web.dev/lcp-lazy-loading/"
@@ -464,7 +464,7 @@ function Home() {
             data.lighthouseResult &&
             data.lighthouseResult.audits["duplicated-javascript"].score !==
               1 && (
-              <FailedAudit
+              <OpportunityAudit
                 title="Remove duplicate modules in JavaScript bundles"
                 description="Remove large, duplicate JavaScript modules from bundles to reduce unnecessary bytes consumed by network activity."
                 link="https://www.oscprofessionals.com/blog/remove-duplicate-javascript-and-css/"
@@ -474,7 +474,7 @@ function Home() {
             data.lighthouseResult &&
             data.lighthouseResult.audits["uses-text-compression"].score !==
               1 && (
-              <FailedAudit
+              <OpportunityAudit
                 title="Enable text compression"
                 description="Text-based resources should be served with compression (gzip, deflate or brotli) to minimize total network bytes."
                 link="https://web.dev/uses-text-compression/"
@@ -484,7 +484,7 @@ function Home() {
             data.lighthouseResult &&
             data.lighthouseResult.audits["cumulative-layout-shift"].score !==
               1 && (
-              <FailedAudit
+              <OpportunityAudit
                 title="Cumulative Layout Shift"
                 description="Cumulative Layout Shift measures the movement of visible elements within the viewport"
                 link="https://web.dev/cls/"
@@ -493,7 +493,7 @@ function Home() {
           {data &&
             data.lighthouseResult &&
             data.lighthouseResult.audits["total-byte-weight"].score !== 1 && (
-              <FailedAudit
+              <OpportunityAudit
                 title="Avoids enormous network payloads"
                 description="Large network payloads cost users real money and are highly correlated with long load times."
                 link="https://web.dev/total-byte-weight/"
@@ -502,7 +502,7 @@ function Home() {
           {data &&
             data.lighthouseResult &&
             data.lighthouseResult.audits["no-document-write"].score !== 1 && (
-              <FailedAudit
+              <OpportunityAudit
                 title="Avoids `document.write()`"
                 description="For users on slow connections, external scripts dynamically injected via `document.write()` can delay page load by tens of seconds."
                 link="https://web.dev/no-document-write/"
