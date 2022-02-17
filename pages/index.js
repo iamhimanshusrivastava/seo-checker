5160176c544dbd2cb03b02aae3d645c98254b93c
Himanshu Srivastava <iamhimanshusrivastava@gmail.com>
2022-02-17 21:54:40 +0530

    added view port height

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ function Home() {
 
   return (
     <Fragment>
-      <div className="flex flex-col">
+      <div className="flex flex-col h-screen">
         <div>
           <div className="flex flex-col w-full p-4 justify-center mt-10  drawer-end">
             <div className="text-center text-8xl font-Poppins xxxs:text-6xl xxs:text-6xl xs:text-6xl font-extrabold text-transparent bg-clip-text bg-[#343a40]">
